41a4795ac31e6b611016152b981c46a65d854ba2
Clara <claralanducci66@gmail.com>
2026-06-23 21:58:27 -0300

    Alterações Mínimas

--- public/components/login.js
+++ public/components/login.js
@@ -1,12 +1,36 @@
 const frame = document.getElementById('frame');
-
 const goCad = document.getElementById('goCad');
 const goLogin = document.getElementById('goLogin');
-
+const polygon = document.querySelector('.polygon');
 goCad.addEventListener('click', () => {
   frame.classList.add('flipped');
+  polygonSide();
+    moveFrame('right');
 });
 
 goLogin.addEventListener('click', () => {
   frame.classList.remove('flipped');
+  polygonSide();
+  moveFrame('left');
 });
+
+function moveFrame(side) {
+  if (side === 'left') {
+    frame.style.left = '10%';
+    frame.style.right = 'auto';
+  } else {
+    frame.style.right = '10%';
+    frame.style.left = 'auto';
+  }
+}
+function polygonSide() {
+  if (frame.classList.contains('flipped')) {
+    polygon.style.right = 'auto';
+    polygon.style.left = '0';
+    polygon.style.transform = 'scaleX(-1)';
+  } else {
+    polygon.style.left = 'auto';
+    polygon.style.right = '0';
+    polygon.style.transform = 'scaleX(1)';
+  }
+}
