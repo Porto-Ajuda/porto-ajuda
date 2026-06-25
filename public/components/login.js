2dfc19b33759b3fbf89c848ed620483e05acee2a
Clara <claralanducci66@gmail.com>
2026-06-24 23:23:27 -0300

    leves alterações

--- public/components/login.js
+++ public/components/login.js
@@ -2,10 +2,12 @@ const frame = document.getElementById('frame');
 const goCad = document.getElementById('goCad');
 const goLogin = document.getElementById('goLogin');
 const polygon = document.querySelector('.polygon');
+const moon = document.getElementById('moonT');
 goCad.addEventListener('click', () => {
   frame.classList.add('flipped');
+   movePolygon();
     moveFrame('right');
-    movePolygon();
+    
 });
 
 goLogin.addEventListener('click', () => {
@@ -21,5 +23,19 @@ function moveFrame(side) {
   } else {
     frame.style.right = '10%';
     frame.style.left = 'auto';
+  }
+}
+function movePolygon() {
+  if (frame.classList.contains('flipped')) {
+    polygon.style.transform = 'translateX(-250%) scaleX(1)';
+     moon.style.transform = 'translateX(-410%) scaleX(1)';
+    polygon.addEventListener('transitionend', function handler(e) {
+      if (e.propertyName !== 'transform') return;
+
+      polygon.removeEventListener('transitionend', handler);
+      moon.removeEventListener('transitionend', handler);
+      polygon.style.transform = 'translateX(-150%) scaleX(-1)';
+       moon.style.transform = 'translateX(-155%) scaleX(1)';
+    });
   }
 }
