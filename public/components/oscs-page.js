e75f573ade31263d9b6351a526d21cb07aa9340f
murilo l. lima <murillima201208@gmail.com>
2026-06-27 00:23:43 -0300

    tentativa falha de criar a página de ongs

--- public/components/oscs-page.js
+++ public/components/oscs-page.js
@@ -1,1 +1,16 @@
-*
+
+document.addEventListener("DOMContentLoaded", () => {
+
+            const elementosSurgir = document.querySelectorAll(".surgir");
+
+            const observador = new IntersectionObserver((entradas) => {
+                entradas.forEach(entrada => {
+                    if (entrada.isIntersecting) {
+                        entrada.target.classList.add("active");
+                    }
+                });
+            }, { threshold: 0.15 });
+
+            elementosSurgir.forEach(el => observador.observe(el));
+
+        });
