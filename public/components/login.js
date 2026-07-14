3e9c484a10c009d6a7652bf5222beb05d27b0fce
Clara <claralanducci66@gmail.com>
2026-07-14 19:44:35 -0300

    mudanças nos formulários

--- public/components/login.js
+++ public/components/login.js
@@ -1,41 +1,69 @@
 const frame = document.getElementById('frame');
 const goCad = document.getElementById('goCad');
 const goLogin = document.getElementById('goLogin');
-const polygon = document.querySelector('.polygon');
-const moon = document.getElementById('moonT');
+const polygon = document.querySelector('.shape');
+const CPF = document.getElementById('cpf');
 goCad.addEventListener('click', () => {
   frame.classList.add('flipped');
-   movePolygon();
     moveFrame('right');
     
 });
 
 goLogin.addEventListener('click', () => {
   frame.classList.remove('flipped');
-  movePolygon();
   moveFrame('left');
 });
 
 function moveFrame(side) {
   if (side === 'left') {
-    frame.style.left = '10%';
+    frame.style.left = '15%';
     frame.style.right = 'auto';
   } else {
-    frame.style.right = '10%';
+    frame.style.right = '50%';
     frame.style.left = 'auto';
   }
 }
-function movePolygon() {
-  if (frame.classList.contains('flipped')) {
-    polygon.style.transform = 'translateX(-250%) scaleX(1)';
-     moon.style.transform = 'translateX(-410%) scaleX(1)';
-    polygon.addEventListener('transitionend', function handler(e) {
-      if (e.propertyName !== 'transform') return;
+cpf.addEventListener('input', () => {
+  const cpfValue = cpf.value.replace(/\D/g, '');
+  cpf.value = cpfValue.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+});
+//** transição do cadastro **//
+const nextBtn = document.getElementById('nextBtn');
 
-      polygon.removeEventListener('transitionend', handler);
-      moon.removeEventListener('transitionend', handler);
-      polygon.style.transform = 'translateX(-150%) scaleX(-1)';
-       moon.style.transform = 'translateX(-155%) scaleX(1)';
-    });
-  }
-}
+const rows = [
+    document.querySelector('.row2'),
+    document.querySelector('.row3'),
+    document.querySelector('.row4')
+];
+
+let etapa = 0;
+
+rows.forEach((row, index) => {
+    row.style.display = index === 0 ? 'grid' : 'none';
+});
+
+
+nextBtn.addEventListener('click', () => {
+    rows[etapa].style.display = 'none';
+    etapa++;
+
+    if (etapa < rows.length) {
+
+        rows[etapa].style.display = 'grid';
+
+        nextBtn.style.display =
+            etapa === rows.length - 1 ? 'none' : 'block';
+
+        cad.style.display =
+            etapa === rows.length - 1 ? 'block' : 'none';
+    }
+
+});const form = document.querySelector('.formulario');
+const termos = document.getElementById('termos');
+
+form.addEventListener('submit', (e) => {
+    if (!termos.checked) {
+        e.preventDefault(); 
+       
+    }
+});
