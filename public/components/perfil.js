3b9f2d02d06a667b91cbe60f8fb0e2c11d64d56e
Clara <claralanducci66@gmail.com>
2026-09-09 20:25:00 -0300

    correções

--- public/components/perfil.js
+++ public/components/perfil.js
@@ -24,8 +24,23 @@ const DetalhesAcessibilidade = document.getElementsByClassName("detalhes-acessib
 
 Seta[0].addEventListener("click", () => {
     DetalhesUsuario[0].classList.toggle("active");
+    DetalhesAcessibilidade[0].classList.remove("active");
 });
 
 Seta[1].addEventListener("click", () => {
     DetalhesAcessibilidade[0].classList.toggle("active");
+    DetalhesUsuario[0].classList.remove("active");
 });     
+const TogLB = document.getElementById('libra');
+const TogAC = document.getElementById('contraste');
+
+const circleLB = TogLB.querySelector('.toggle-circle');
+const circleAC = TogAC.querySelector('.toggle-circle');
+TogLB.addEventListener('click', () => {
+    circleLB.classList.toggle('active');
+    
+});
+
+TogAC.addEventListener('click', () => {
+    circleAC.classList.toggle('active');
+});
