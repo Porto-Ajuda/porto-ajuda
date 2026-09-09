8ab6de5a86b59dcf44bf4fc9a21450b85e574256
Clara <claralanducci66@gmail.com>
2026-09-09 19:08:30 -0300

    acréscimos

--- public/components/perfil.js
+++ public/components/perfil.js
@@ -17,3 +17,15 @@ btn.addEventListener("click", () => {
        setItemsToLocalStorage();
     }
 });
+const Seta = document.getElementsByClassName("btn-seta");
+const Cards = document.getElementsByClassName("cards-container");
+const DetalhesUsuario = document.getElementsByClassName("detalhes-usuario");
+const DetalhesAcessibilidade = document.getElementsByClassName("detalhes-acessibilidade");
+
+Seta[0].addEventListener("click", () => {
+    DetalhesUsuario[0].classList.toggle("active");
+});
+
+Seta[1].addEventListener("click", () => {
+    DetalhesAcessibilidade[0].classList.toggle("active");
+});     
