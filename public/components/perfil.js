2795072c28637fec0862d8c98ba62138492ec297
Clara <claralanducci66@gmail.com>
2026-09-13 21:02:40 -0300

    tentativa de responsividade

--- public/components/perfil.js
+++ public/components/perfil.js
@@ -1,36 +1,71 @@
+// =========================
+// EDITAR DADOS DO PERFIL
+// =========================
 
-// Script para editar os dados do perfil  
 const btn = document.querySelector("#btn");
 const inputs = document.querySelectorAll(".info-perfil input");
+
+const infoPerfil = document.querySelector(".info-perfil");
+const rectangle = document.querySelector(".rectangle");
+
 let editando = false;
+
+
 btn.addEventListener("click", () => {
+
+    // Inverte o estado
     editando = !editando;
+
+
+    // Habilita ou desabilita os inputs
     inputs.forEach(input => {
         input.disabled = !editando;
     });
 
+
+    // Mostra/esconde a senha
+    infoPerfil.classList.toggle("editando", editando);
+
+
+    // Aumenta/diminui o rectangle
+    rectangle.classList.toggle("editando", editando);
+
+
+    // Altera o texto do botão
     if (editando) {
+
         btn.textContent = "Salvar Dados";
+
     } else {
+
         btn.textContent = "Alterar Dados";
 
-       setItemsToLocalStorage();
+        setItemsToLocalStorage();
     }
+
 });
 const Seta = document.getElementsByClassName("btn-seta");
 const Cards = document.getElementsByClassName("cards-container");
 const DetalhesUsuario = document.getElementsByClassName("detalhes-usuario");
 const DetalhesAcessibilidade = document.getElementsByClassName("detalhes-acessibilidade");
-
+const Configuracoes = document.getElementsByClassName("detalhes-configuracoes");
 Seta[0].addEventListener("click", () => {
     DetalhesUsuario[0].classList.toggle("active");
     DetalhesAcessibilidade[0].classList.remove("active");
+    Configuracoes[0].classList.remove("active");
 });
 
 Seta[1].addEventListener("click", () => {
     DetalhesAcessibilidade[0].classList.toggle("active");
     DetalhesUsuario[0].classList.remove("active");
+    Configuracoes[0].classList.remove("active");
+
 });     
+Seta[2].addEventListener("click", () => {
+    DetalhesAcessibilidade[0].classList.remove("active");
+    DetalhesUsuario[0].classList.remove("active");
+    Configuracoes[0].classList.toggle("active");
+});
 const TogLB = document.getElementById('libra');
 const TogAC = document.getElementById('contraste');
 
@@ -44,3 +79,16 @@ TogLB.addEventListener('click', () => {
 TogAC.addEventListener('click', () => {
     circleAC.classList.toggle('active');
 });
+const usuarioLang = document.querySelector(".Ulang");
+
+let NavLang = navigator.language.split("-")[0];
+
+if (NavLang === "pt") {
+    usuarioLang.value = "Português";
+}
+else if (NavLang === "en") {
+    usuarioLang.value = "English";
+}
+else if (NavLang === "es") {
+    usuarioLang.value = "Español";
+}
