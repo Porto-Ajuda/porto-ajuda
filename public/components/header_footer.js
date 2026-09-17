f4bb5702dd2765a64c414b86d93e03f2a4e7f92a
Murilo mexicano <murillima201208@gmail.com>
2026-09-17 00:03:29 -0300

    criação dos arquivos das novas páginas

--- public/components/header_footer.js
+++ public/components/header_footer.js
@@ -44,7 +44,7 @@ const cabecalho = `
 <li id="barra">|</li>
       <li><a href="#">Ajuda</a></li>
 <li id="barra">|</li>
-      <li><a href="#">Apoiar o Site</a></li>
+      <li><a href="apoie.html">Apoiar o Site</a></li>
 <li id="barra">|</li>
       <li><a href="proximity.html">ONG's Regionais</a></li>
       </ul>
